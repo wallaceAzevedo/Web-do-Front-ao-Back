a4f5c5b11676a4d8a2978d8abeb0a6ccfa3cca74
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-30 15:09:53 -0300

    home
    adicao de imagens e carousel com react

--- src/components/head.js
+++ src/components/head.js
@@ -3,8 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
     return(
-        <nav style={{height:75}} class =" bg-dark col-12 mb-3">
-           
+        <nav style={{height:75}} class =" bg-dark mb-3 navbar navbar-expand-lg navbar-dark bg-dark ">
+          <h2 style={{fontFamily:'ink free'}} class="text-light p-3">Forum GameDeveloper</h2> 
+          <div id="menu" class="ml-auto">
+            <ul style={{display:'inline'}}>
+                <li class="ml-3"><a href="#">Home</a></li>
+                <li class="ml-3"><a href="#">Registro</a></li>
+                <li class="ml-3"><a href="#">Login</a></li>
+            </ul>
+          </div>          
         </nav>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
+import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Head from '../components/head'
-import React from 'react';
+import Head from '../components/head';
+import image1 from'../img/kimetsu.jpg';
+import image2 from'../img/zetsu2.png';
+import image3 from'../img/zetsu.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel} from 'react-responsive-carousel';
 
 function home(){
     return(
         <React.Fragment>
         <Head></Head>
+        <Carousel>
+            <div>
+                <img src={image1} />
+                <p className="legend">imagem 1</p>
+            </div>
+            <div>
+                <img src={image2} />
+                <p className="legend">imagem 2</p>
+            </div>
+                <div>
+                <img src={image3} />
+                <p className="legend">imagem 3</p>
+            </div>
+        </Carousel>
         <div class ="container bg-dark" style={{height: '85vh'}}>
             <h1 class="text-success">Hello world !!!</h1>
         </div>
