9f326fdac46b40da1ea9f9d0e537b7b400e95d1e
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-30 16:54:07 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Routes from './route';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'popper.js/dist/popper';
+import 'jquery/dist/jquery';
 
 function App() {
   return (
--- src/components/head.js
+++ src/components/head.js
@@ -1,19 +1,29 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../media.css';
+import icon from '../img/icon.png'
 
 function App(){
     return(
-        <nav style={{height:75}} class =" bg-dark mb-3 navbar navbar-expand-lg navbar-dark bg-dark ">
-          <h2 style={{fontFamily:'ink free'}} class="text-light p-3">Forum GameDeveloper</h2> 
-          <div id="menu" class="ml-auto">
-            <ul style={{display:'inline'}}>
-                <li class="ml-3"><a href="#">Home</a></li>
-                <li class="ml-3"><a href="#">Registro</a></li>
-                <li class="ml-3"><a href="#">Login</a></li>
-            </ul>
-          </div>          
-        </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand text-warning" href="/"><img src={icon}></img></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon "></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+              <a class="nav-link hoverItem text-warning border-left border-right border-warning" href="/register"><p class="itemText">Register</p></a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link hoverItem text-warning border-left border-right border-warning" href="/login"><p class="itemText">Login</p></a>
+          </li>
+          </ul>
+      </div>
+  </nav>
     )
 }
 
 export default App;
+
