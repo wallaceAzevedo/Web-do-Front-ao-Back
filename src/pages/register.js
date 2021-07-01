361c46e4664c0d3bc91c19779090117ef6c992b5
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-30 21:05:45 -0300

    .

--- src/pages/register.js
+++ src/pages/register.js
@@ -5,66 +5,7 @@ import Head from '../components/head';
 import { render } from '@testing-library/react';
 import { Redirect } from 'react-router';
 
-export default class Cadastro extends Component{
-
-  constructor(props){
-    super(props);
-    this.state ={
-       name:'',
-       email:'',
-       senha:'',
-       senha2: '',
-       redirect: false,
-    }
-
-
-    this.cadastrar = this.cadastrar.bind(this);
-  }
-  cadastrar(e){ 
-    if(this.state.senha != this.state.senha2){
-      alert("Iforme duas senhas iguais!")
-    }else{
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-
-      var raw = JSON.stringify({"name":this.state.name,"email":this.state.email,"senha":this.state.senha,"image":"https://i.pinimg.com/564x/50/93/3b/50933b3d277a46befc38b321b979a04f.jpg"});
-      var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-
-      fetch("http://localhost:8080/register", requestOptions)
-        .then(response => response.json())
-        .then(response =>{
-          if(response == 'Já existe esse email'){
-            alert('Já existe esse email cadastrado!!!')
-          }else{
-            this.setState({redirect:true})
-            alert('Cadastrado com sucesso!!!')
-            
-          }
-        })
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-      }
-    
-
-      
-
-        e.preventDefault()
-
-  }
-
-render(){
-  if(this.state.redirect === true){
-    return <Redirect
-    to={{
-      pathname: "/login",  
-    }}
-      />
-  }
+function register () {
   return (
     <React.Fragment>
     <Head></Head>
@@ -77,4 +18,5 @@ render(){
     </React.Fragment>
   );
 }
-}
+
+export default register;
--- src/route.tsx
+++ src/route.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import jogos from './pages/Categorias';
 import home from './pages/Home';
+import login from './pages/login';
+import register from './pages/register';
 
 function route (){
     return (
         <BrowserRouter>
             <Route path="/" exact component = {home}/>
             <Route path="/categoria" component = {jogos}/>
+            <Route path="/login" component = {login}/>
+            <Route path="/register" component = {register}/>
         </BrowserRouter>
     )
 }
